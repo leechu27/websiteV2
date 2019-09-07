038384f20b0b41a395770096fb22c95839c7259f
Chun Ho Lee <chunho.lee@mail.utoronto.ca>
2019-09-07 13:06:37 -0400

    fixed resolution issues

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Chun Ho Lee</title>
+    </head>
+    <body>
+        <?php
+        $myvar = "this is my variable";
+        echo $myvar'
+        ?>
+    </body>
+</html>
